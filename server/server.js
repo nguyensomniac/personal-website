125132f03008aa6bf22eaa8f7feff262f8c48f7b
Lily Nguyen <lanchi.v.nguyen@gmail.com>
2015-04-04 04:21:11 -0700

    add meteor settings for production

--- server/server.js
+++ server/server.js
@@ -1,9 +1,5 @@
 if (Meteor.isServer) {
   Meteor.startup(function () {
-      if(process.env.METEOR_ENV = 'production') {
-        Meteor.settings = process.env.METEOR_SETTINGS;
-      }
-      console.log(process.env);
       console.log(Meteor.settings)
   });
 }
